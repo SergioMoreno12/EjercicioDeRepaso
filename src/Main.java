02cab1a3f777eb1c6b205ea2c67da280bf143d20
SergioMoreno12 <ssmoreno@ucundinamarca.edu.co>
2025-02-19 22:46:29 -0500

    Suma de dos numeros

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Suma de dos numeros
+        double numero1 = 5;
+        double numero2 = 6;
+
+        // suma
+        double suma = numero1 + numero2;
+
+        // resultado
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
