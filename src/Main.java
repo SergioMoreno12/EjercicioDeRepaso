a05699a4ad22c686d62c090b29c9eb574deddf8a
SergioMoreno12 <ssmoreno@ucundinamarca.edu.co>
2025-02-19 23:04:21 -0500

    Se agrego la resta

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,18 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-        //Suma de dos numeros
+        //numeros
         double numero1 = 5;
         double numero2 = 6;
 
         // suma
         double suma = numero1 + numero2;
 
+        // resta
+        double resta = numero1 - numero2;
+
         // resultado
         System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+        System.out.println("La resta de " + numero1 + " y " + numero2 + " es: " + resta);
     }
 }
